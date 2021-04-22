e9182b02e290c40f8bb5362419663b8bcfa801c3
Alexander Braginsky <bro@beam-mw.com>
2021-04-22 12:13:35 +0300

    plugin init commit

---
+++ html/scripts/index.js
@@ -0,0 +1,177 @@
+import Utils from "./utils.js"
+
+const GROTHS_IN_BEAM = 100000000;
+const CONTRACT_ID = "";
+
+class Faucet {
+    constructor() {
+        this.timeout = undefined;
+        this.pluginData = {
+            inTransaction: false,
+            locked_demoX: 0,
+            locked_beams: 0,
+            stake: 0,
+            contractBytes: null
+        }
+    }
+
+    setError = (errmsg) => {
+        Utils.hide('faucet')
+        Utils.setText('error', errmsg)
+        if (this.timeout) {
+            clearTimeout(this.timeout)   
+        }
+        this.timeout = setTimeout(() => {
+            Utils.setText('error', "")
+            this.start();
+        }, 3000);
+    }
+
+    start = () => {
+        Utils.download("./daoManager.wasm", (err, bytes) => {
+            if (err) {
+                let errTemplate = "Failed to load shader,";
+                let errMsg = [errTemplate, err].join(" ");
+                return this.setError(errMsg);
+            }
+
+            this.pluginData.contractBytes = bytes;
+    
+            Utils.callApi("view_params", "invoke_contract", {
+                contract: bytes,
+                create_tx:false,
+                args: "role=manager,action=view_params,cid=" + CONTRACT_ID
+            })
+        })
+    }
+
+    loadStake = () => {
+        Utils.callApi("view_stake", "invoke_contract", {
+            contract: this.pluginData.contractBytes,
+            create_tx:false,
+            args: "role=manager,action=view_stake,cid="
+        })
+    }
+    
+    refresh = (now) => {
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+        }
+        this.timeout = setTimeout(() => {
+            Utils.callApi("view_params", "invoke_contract", {
+                contract: this.pluginData.contractBytes,
+                create_tx:false,
+                args: "role=manager,action=view_params,cid=" + CONTRACT_ID
+            })
+        }, now ? 0 : 3000)
+    }
+    
+    parseShaderResult = (apiResult) => {
+        if (typeof(apiResult.output) != 'string') {
+            throw "Empty shader response";
+        }
+    
+        let shaderOut = JSON.parse(apiResult.output)
+        if (shaderOut.error) {
+            throw ["Shader error: ", shaderOut.error].join("")
+        }
+    
+        return shaderOut
+    }
+
+    showStaking = () => {
+        Utils.show('faucet');
+        Utils.setText('deposited-amount', this.pluginData.stake / GROTHS_IN_BEAM);
+        Utils.setText('total-beam-amount', this.pluginData.locked_beams / GROTHS_IN_BEAM);
+        Utils.setText('demox-amount', this.pluginData.locked_demoX / GROTHS_IN_BEAM);
+        this.refresh(false);
+    }
+
+    onApiResult = (json) => {    
+        try {
+            const apiAnswer = JSON.parse(json);
+            if (apiAnswer.error) {
+                throw JSON.stringify(apiAnswer.error)
+            }
+    
+            const apiCallId = apiAnswer.id;
+            const apiResult = apiAnswer.result;
+            if (!apiResult) {
+                throw "Failed to call wallet API"
+            }
+
+            if (apiCallId == "view_params") {
+                let shaderOut = this.parseShaderResult(apiResult);
+                this.pluginData.locked_demoX = shaderOut.params['locked_demoX'];
+                this.pluginData.locked_beams = shaderOut.params['locked_beams'];
+                this.loadStake();
+            }
+
+            if (apiCallId == "view_stake") {
+                let shaderOut = this.parseShaderResult(apiResult);
+                this.pluginData.stake = shaderOut['stake'];
+                this.showStaking();
+            }
+    
+            if (apiCallId == "lock") {
+                return this.refresh(true)
+            }
+        }
+        catch(err) 
+        {
+            return this.setError(err.toString())
+        }
+    }
+}
+
+Utils.onLoad(async (beamAPI) => {
+    let faucet = new Faucet();
+    Utils.getById('error').style.color = beamAPI.style.validator_error;
+    beamAPI.api.callWalletApiResult.connect(faucet.onApiResult);
+    faucet.start();
+
+    Utils.getById('deposit').addEventListener('click', (ev) => {
+        Utils.show('deposit-popup');
+    });
+    
+
+    Utils.getById('cancel-button-popup-dep').addEventListener('click', (ev) => {
+        Utils.hide('deposit-popup');
+    });
+
+    Utils.getById('deposit-input').addEventListener('keydown', (event) => {
+        const specialKeys = [
+            'Backspace', 'Tab', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowUp',
+            'Control', 'Delete', 'F5'
+          ];
+
+        if (specialKeys.indexOf(event.key) !== -1) {
+            return;
+        }
+
+        const current = Utils.getById('deposit-input').value;
+        const next = current.concat(event.key);
+      
+        if (!Utils.handleString(next)) {
+            event.preventDefault();
+        }
+    })
+
+    Utils.getById('deposit-input').addEventListener('paste', (event) => {
+        const text = event.clipboardData.getData('text');
+        if (!Utils.handleString(text)) {
+            event.preventDefault();
+        }
+    })
+
+    Utils.getById('deposit-button-popup').addEventListener('click', (ev) => {
+        const bigValue = new Big(Utils.getById('deposit-input').value);
+        const value = bigValue.times(GROTHS_IN_BEAM);
+        Utils.callApi("lock", "invoke_contract", {
+            args: `role=manager,action=lock,amount=${parseInt(value)},cid=${CONTRACT_ID}`
+        });
+        Utils.hide('deposit-popup');
+        ev.preventDefault();
+        return false;
+    });
+});
